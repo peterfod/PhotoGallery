f3cfa272165c884a7081b02247f515db1862216f
peter <peterfod@freemail.hu>
2023-06-10 20:59:10 +0200

    modified:   app/Http/Controllers/PhotoController.php

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -7,49 +7,50 @@ use DB;
 
 class PhotoController extends Controller
 {
-	private $fotok_tabla = 'photos';
-	
-    public function create($gallery_id) {
+    private $fotok_tabla = 'photos';
+
+    public function create($gallery_id)
+    {
         //die('PHOTO/CREATE');
         return view('photo/create', compact('gallery_id'));
     }
-    
-	public function store(Request $request) {
-		$cim = $request->input('cim');
-		$leiras = $request->input('leiras');
-		$helyszin = $request->input('helyszin');
-		$kep = $request->file('kep');
-		$galeria = $request->input('galeria');
-		$tulajdonos = 1;
 
-		if ($kep) {
-		  $fajlnev = $kep->getClientOriginalName();
-		  $kep->move(public_path('fotok/'.$galeria), $fajlnev);
+    public function store(Request $request)
+    {
+        $cim = $request->input('cim');
+        $leiras = $request->input('leiras');
+        $helyszin = $request->input('helyszin');
+        $kep = $request->file('kep');
+        $galeria = $request->input('galeria');
+        $tulajdonos = 1;
 
-		  DB::table($this->fotok_tabla)->insert(
-			[
-			  'title' => $cim,
-			  'description' => $leiras,
-			  'location' => $helyszin,
-			  'image' => $fajlnev,
-			  'gallery_id' => $galeria,
-			  'owner_id' => $tulajdonos
-			]  
-		  );
-		  //EZ NEM MŰKÖDIK!!!
-		  //return \Redirect::route('gallery.show', array('id'=>$galeria))->with('uzenet', 'A fotót sikeresen feltöltötted!');
-		  return \Redirect::route('gallery.show', $galeria)->with('uzenet', 'A fotót sikeresen feltöltötted!');
+        if ($kep) {
+            $fajlnev = $kep->getClientOriginalName();
+            $kep->move(public_path('fotok/' . $galeria), $fajlnev);
 
-		}
-		else {
-		  return \Redirect::route('gallery.show', [$galeria])->with('uzenet', 'A fotó feltöltése nem sikerült!');
-		}
-	}    
+            DB::table($this->fotok_tabla)->insert(
+                [
+                    'title' => $cim,
+                    'description' => $leiras,
+                    'location' => $helyszin,
+                    'image' => $fajlnev,
+                    'gallery_id' => $galeria,
+                    'owner_id' => $tulajdonos
+                ]
+            );
+            //EZ NEM MŰKÖDIK!!!
+            //return \Redirect::route('gallery.show', array('id'=>$galeria))->with('uzenet', 'A fotót sikeresen feltöltötted!');
+            return \Redirect::route('gallery.show', $galeria)->with('uzenet', 'A fotót sikeresen feltöltötted!');
+        } else {
+            return \Redirect::route('gallery.show', [$galeria])->with('uzenet', 'A fotó feltöltése nem sikerült!');
+        }
+    }
 
 
-    public function details($id) {
+    public function details($id)
+    {
         //die($id);
-      $photo = DB::table($this->fotok_tabla)->where('id',$id)->first();
-      return view('photo/details', compact('photo'));
+        $photo = DB::table($this->fotok_tabla)->where('id', $id)->first();
+        return view('photo/details', compact('photo'));
     }
 }
